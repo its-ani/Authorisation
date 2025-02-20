9483357aae8fe873a62db20958eab8f95d5d2041
its-ani <guptaanirudh712@gmail.com>
2025-02-21 01:50:49 +0530

    FInal

--- src/main/java/com/example/userservicefeb25/Configuration/MyAppConfig.java
+++ src/main/java/com/example/userservicefeb25/Configuration/MyAppConfig.java
@@ -5,16 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class MyAppConfig {
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//@Configuration
+//public class MyAppConfig {
+//
+//    @Bean
+//    public PasswordEncoder getPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
